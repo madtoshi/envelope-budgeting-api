61180337a2c51ba93e6abfb95805032631261fe7
madtoshi <106340158+madtoshi@users.noreply.github.com>
2022-10-19 14:31:14 -0400

    Added envelopes and a post route

--- index.js
+++ index.js
@@ -7,6 +7,41 @@ app.get('/', (req, res, next) => {
     res.send('<h1>Hello, World.</h1>');
 });
 
+const envelopes = [
+    {
+        id: 1,
+        name: mortgage,
+        monthlyAllocation: 0,
+        allocationRemaining: 0,
+    },
+    {
+        id: 2,
+        name: gas,
+        monthlyAllocation: 0,
+        allocationRemaining: 0,
+    },
+    {
+        id: 3,
+        name: food,
+        monthlyAllocation: 0,
+        allocationRemaining: 0,
+    }
+];
+
+const nextEnvelopeId = 3
+
+app.post('/envelopes', (req, res, next) => {
+    const newEnvelope = req.body;
+    if (!newEnvelope.name || !newEnvelope.monthlyAllocation) {
+        res.status(400).send('Please include a name and monthly allocation.')
+    } else {
+        newEnvelope.id = nextEnvelopeId++;
+        newEnvelope.allocationRemaining = newEnvelope.monthlyAllocation;
+        envelopes.push(newEnvelope);
+        res.send(newEnvelope);
+    }
+});
+
 app.listen(3000, () => {
     console.log("Server is listening");
 });
